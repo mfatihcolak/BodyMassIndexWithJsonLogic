7fb210f2478b6435ae0d13cd5df3032a4eddbec8
Muhammed Fatih Çolak <mfatihcolak1@gmail.com>
2022-09-13 18:09:01 +0300

    Add project files.

---
+++ JsonLogic.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class JsonLogic
+{
+	public JsonLogic()
+	{
+
+	}
+}
---
+++ JsonLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JsonLogic.Net;
+using Newtonsoft.Json.Linq;
+
+
+var person = @"{
+    'weight':'Your weight',
+    'height':'Your height'
+}";
+
+var rule = @"{
+        '/':[
+        {'var':'weight'},
+        {
+         '*': [
+        {'var':'height'},
+        {'var':'height'}
+        ]}
+]
+}";
+
+
+JToken jt = JToken.Parse(rule);
+JToken jt2 = JToken.Parse(person);
+
+JsonLogicEvaluator jsonLogic = new(EvaluateOperators.Default);
+
+Object result = jsonLogic.Apply(jt, jt2);
+
+Console.WriteLine("Your Body Mass Index: "+ result);
+
